c57b64fbf45d1306272a8375ea15d5939b4c5c74
ThiagoFazzi <thiago@xtwo.com.br>
2019-07-18 11:45:44 +0200

    added app.js to start the server

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import graphqlHttp from 'express-graphql'
+import mongoose from 'mongoose'
+
+
+const app = express()
+app.use(bodyParser.json())
+
+app.use(
+  'graphql',
+  graphqlHttp({
+    schema: 'graphqlSchema',
+    rootValue: 'graphqlResolvers',
+    graphiql: true
+  })
+)
+
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+  }@cluster0-vpwhf.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+).then(
+  app.listen(3000)
+).catch(err => {
+  throw err
+})
