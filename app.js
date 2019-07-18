92ec45cf6d7c5b2ba50c716ca30d45a681c34965
ThiagoFazzi <thiago@xtwo.com.br>
2019-07-18 12:01:52 +0200

    linked resolvers and schemas to app.js

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ import bodyParser from 'body-parser'
 import graphqlHttp from 'express-graphql'
 import mongoose from 'mongoose'
 
+import { graphqlSchema } from './src/graphql/schema'
+import { graphqlResolvers } from './src/graphql/resolvers'
+
 const app = express()
 app.use(bodyParser.json())
 
 app.use(
   'graphql',
   graphqlHttp({
-    schema: 'graphqlSchema',
-    rootValue: 'graphqlResolvers',
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
     graphiql: true
   })
 )
